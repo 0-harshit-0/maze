701a29429dd3f03c667090c8bc84547b7b402b11
harshit singhal <harshitw3b@gmail.com>
2022-01-09 11:25:57 +0530

    c

--- mg.js
+++ mg.js
@@ -1,18 +1,53 @@
 
-var fr = document.querySelector('#fr');
-var cs = parseInt(document.querySelector('#s').value);
-var lc = document.querySelector('#lc');
-var c = document.querySelector('#c');
+var fr = document.querySelector('#fr');  //frame rate
+var cs = parseInt(document.querySelector('#s').value);  //cell
+var r = document.querySelector('#r');  //ratio
+var lc = document.querySelector('#lc');  //line clr
+var c = document.querySelector('#c');  //search clr
+var gen = document.querySelector('#generate');
 
+r.addEventListener('change', ()=> {
+	gen.click();
+});
 
 var canvas = document.querySelector('#canvas');
 var ctx = canvas.getContext('2d');
 let timeout = false;
-function getDimensions() {
-	canvas.width = parseInt(getComputedStyle(document.querySelector('.rightCont')).width);
-	canvas.height = parseInt(getComputedStyle(document.querySelector('.rightCont')).height);
 
+let vs = new VecShapes(ctx), s = new Shapes(ctx);
+let store = new Array();
+let stk = new Stack(), animateStore = [];
+let b = 0, inter;
+
+function getDimensions() {
+	clearInterval(inter);
+	b = 0;
+	cs = parseInt(document.querySelector('#s').value);  //cell
+	let scale = 90;
+	if(r.checked) {
+		let wh = Math.min(parseInt(getComputedStyle(document.querySelector('.rightCont')).width)*scale/100, parseInt(getComputedStyle(document.querySelector('.rightCont')).height)*95/100);
+		if(wh%cs) {
+			wh -= wh%cs;
+		}
+		canvas.width = wh;
+		canvas.height = wh;
+	}else{
+		let w = parseInt(getComputedStyle(document.querySelector('.rightCont')).width)*scale/100;
+		let h = parseInt(getComputedStyle(document.querySelector('.rightCont')).height)*scale/100;
+		if(w%cs) {
+			w -= w%cs;
+		}
+		if(h%cs) {
+			h -= h%cs;
+		}
+		canvas.width = w;
+		canvas.height = h;
+	}
+	
+	stk = new Stack(), animateStore = [];
+	store.length = 0;
 	make(animate);
+	inter = setInterval(animate, parseInt(fr.value));
 }
 
 addEventListener('resize', function(e) {//debounce
@@ -20,53 +55,125 @@ addEventListener('resize', function(e) {//debounce
 	timeout = setTimeout(getDimensions, 500);
 });
 
-let vs = new VecShapes(ctx);
-let s = new Shapes(ctx);
 
 
 class Cells{
-	constructor(x, y, length, sclr, fclr) {
+	constructor(id, x, y, length) {
+		this.id = id;
 		this.pos = new Vector2D(x, y);
 		this.l = length;
-		this.sclr = sclr;
-		this.fclr = fclr;
+		this.sclr = '#302929';
+		this.fclr = lc.value;
+		this.walls = [1,1,1,1];
 	}
 	draw() {
 		vs.box(this.pos, this.l, this.l);
 		vs.fill(this.fclr);
 	}
 	drawWalls() {
-		s.line(this.pos.x, this.pos.y, this.pos.x+this.l, this.pos.y);
-		s.stroke(this.sclr);
-
-		s.line(this.pos.x+this.l, this.pos.y, this.pos.x+this.l, this.pos.y+this.l);
-		s.stroke(this.sclr);
-
-		s.line(this.pos.x+this.l, this.pos.y+this.l, this.pos.x, this.pos.y+this.l);
-		s.stroke(this.sclr);
-
-		s.line(this.pos.x, this.pos.y+this.l, this.pos.x, this.pos.y);
-		s.stroke(this.sclr);
+		ctx.lineCap = 'square';
+		if(this.walls[0]) {
+			s.line(this.pos.x, this.pos.y, this.pos.x+this.l, this.pos.y); //top
+			s.stroke(this.sclr, cs/2);
+		}
+		if(this.walls[1]) {
+			s.line(this.pos.x+this.l, this.pos.y, this.pos.x+this.l, this.pos.y+this.l); //right
+			s.stroke(this.sclr, cs/2);
+		}
+		if(this.walls[2]) {
+			s.line(this.pos.x+this.l, this.pos.y+this.l, this.pos.x, this.pos.y+this.l);  //bottom
+			s.stroke(this.sclr, cs/2);
+		}
+		if(this.walls[3]) {
+			s.line(this.pos.x, this.pos.y+this.l, this.pos.x, this.pos.y);  //left
+			s.stroke(this.sclr, cs/2);
+		}		
+	}
+	removeWalls(chose) {
+		if(this.id-chose == 1) {
+			this.walls[3] = 0;
+			store[chose].walls[1] = 0;
+		}else if(this.id-chose == -1) {
+			this.walls[1] = 0;
+			store[chose].walls[3] = 0;
+		}else if(this.id-chose > 1) {
+			this.walls[0] = 0;
+			store[chose].walls[2] = 0;
+		}else if(this.id-chose < -1) {
+			this.walls[2] = 0;
+			store[chose].walls[0] = 0;
+		}
 	}
 }
 
-let store = new Array();
-function make(callback) {
-	for(let i = 0; i < canvas.width; i+= cs) {
-		for(let j = 0; j < canvas.height; j+= cs) {
-			store.push(new Cells(i, j, cs, 'black', 'white'));
+
+function visitedNeighbours(graph, c) {
+	let nArray = [...graph.neighbors(c)];
+	let remains = [];
+	for (var i = 0; i < nArray.length; i++) {
+		if(!graph.AdjList.get(nArray[i]).visited) {
+			remains.push(nArray[i]);
 		}
 	}
+	return remains;
+}
+
+function DFS(graph, root) {
+	graph.AdjList.get(root).visited = true;
+	stk.push(root);
+	while(stk.length) {
+		let curr = stk.pop();
+		//console.log(curr)
+		animateStore.push(curr);
+		let sathi = [...graph.neighbors(curr)];
+		let available = visitedNeighbours(graph, curr);
+		if(available.length){
+			stk.push(curr);
+			let chosen = available[Math.floor(Math.random()*available.length)];
+			store[curr].removeWalls(chosen);
+			graph.AdjList.get(chosen).visited = true;
+			stk.push(chosen);
+		}
+	}
+}
+
+function make(callback) {
+	let maxW = canvas.width/cs-1;
+	let n = canvas.width/cs*canvas.height/cs;
+	g = new UGraph(n);
+	let count = 0;
+	for(let i = 0; i < canvas.height; i+= cs) {
+		for(let j = 0; j < canvas.width; j+= cs) {
+			store.push(new Cells(count, j, i, cs, 'black', 'white'));
+			if(count+1 <= maxW) {
+				g.add(count, count+1);
+			}
+			if(count+(canvas.width/cs) <= n-1) {
+				g.add(count, count+(canvas.height/cs));
+			}
+			count++;
+		}
+		maxW += canvas.width/cs;
+	}
+	//g.dfs(0)
+	DFS(g, 0);
 	if(callback) callback();
 }
 function animate() {
+	//s.clear(0,0,canvas.width,canvas.height);
+	if(animateStore[b] != undefined) {
+		store[animateStore[b]].draw();
+	}else{
+		console.log('stop')
+		clearInterval(inter);
+	}
 	store.forEach(z=>{
-		z.draw();
 		z.drawWalls();
 	});
+	b++;
+	//console.log(b)
 }
 getDimensions();
-
 
 
 /*
